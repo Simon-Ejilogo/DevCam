1e4d7a0e80ea444292ebf0cf1283d4655a45debf
simlogo <simlogo17@gmail.com>
2022-08-20 13:59:03 +0100

    bootcamp routes

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+//Route files
+const bootcamps = require('./routes/bootcamps')
 
 //Load env vars
 dotenv.config({ path:'./config/config.env' });
 
+
+//initialize app with express
 const app = express();
+
+app.use('/api/v1/bootcamps', bootcamps);
+
 
 const PORT = process.env.PORT || 5000;
 
@@ -13,3 +20,5 @@ app.listen(
     PORT,
     console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
     );
+
+
