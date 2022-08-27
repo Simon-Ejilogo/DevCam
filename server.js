09b2c15e32f8ee1bfc1e9e084909a2c5081e43b7
simlogo <simlogo17@gmail.com>
2022-08-27 22:56:48 +0100

    database configured

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const connectDB = require('./config/db')
 
-//Route files
-const bootcamps = require('./routes/bootcamps')
+
 
 //Load env vars
 dotenv.config({ path:'./config/config.env' });
 
+//connect to database
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamps')
 
 //initialize app with express
 const app = express();
