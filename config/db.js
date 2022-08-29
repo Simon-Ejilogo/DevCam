7bd206be088ec1fe66177f3d25c946b24546ecfa
simlogo <simlogo17@gmail.com>
2022-08-29 22:00:19 +0100

    bootcamp model

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ const connectDB = async () =>{
 
     });
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`.green.underline);
 };
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require ('colors');
 const connectDB = require('./config/db')
 
 
@@ -29,9 +30,16 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
     PORT,
     console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
     );
 
 
+
+//Handle unhandles promise rejection
+process.on('unhandledRejection',(err, promise) =>{
+    console.log (`Error: ${err.message}`.red);
+    //Close server & exit process
+    server.close(() => process.exit(1));
+})
